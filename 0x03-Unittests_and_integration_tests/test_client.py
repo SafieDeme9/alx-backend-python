f610ed86f418f3176e1d15210c301d5536fe3121
SafieDeme9 <safiedeme9@gmail.com>
2024-11-04 23:23:53 +0100

    task4

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,20 +1,20 @@
+#!/usr/bin/python
 import unittest
 from unittest.mock import patch
 from client import GithubOrgClient
 from parameterized import parameterized
 
-class TestGithubOrgClient(unittest.TestCase):
-    """Test GithubOrgClient class"""
 
+class TestGithubOrgClient(unittest.TestCase):
+    """Test Github org client"""
     @parameterized.expand([
-        ('google',),
-        ('abc',)
+        ('google'),
+        ('abc')
     ])
-    @patch('client.get_json', return_value={"mock_key": "mock_value"})
-    def test_org(self, org_name, mock_get_json):
-        """Test that GithubOrgClient.org returns the correct value"""
-        client = GithubOrgClient(org_name)
-        org_data = client.org
-        endpoint = f'https://api.github.com/orgs/{org_name}'
-        mock_get_json.assert_called_once_with(endpoint)
-        self.assertEqual(org_data, {"mock_key": "mock_value"})
+    @patch('client.get_json')
+    def test_org(self, data, mock):
+        """Test org"""
+        endpoint = 'https://api.github.com/orgs/{}'.format(data)
+        spec = GithubOrgClient(data)
+        spec.org()
+        mock.assert_called_once_with(endpoint)
